96df6218d7ecaf971ee54402c2eaeeda7d2993b0
mca-programmer <freelancermca52@gmail.com>
2025-12-17 00:30:41 +0600

    someting update data Petsection

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
       <Toaster
         position="top-center"
         reverseOrder={false}
